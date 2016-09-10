fbc88f9ab9120a468cdeff2647d7ce353881881d
Rizki-Adrian <rizkiadrian39@gmail.com>
2016-09-10 16:21:09 +0700

    Fixed Image Size

--- storage/framework/views/a104e7c87138200e514a93dcc10e38dd2dba6dcf.php
+++ storage/framework/views/a104e7c87138200e514a93dcc10e38dd2dba6dcf.php
@@ -4,6 +4,6 @@
 	<div class = col-xs-4>
 		<h3 class ="bio">Rizki Adrian</h3>
 </br>
-		<img src="https://cdn0.iconfinder.com/data/icons/avatars-3/512/avatar_hiphop_guy-512.png" alt="..." class="img-circle" height = "50%" width="50%">
+		<img src="https://cdn0.iconfinder.com/data/icons/avatars-3/512/avatar_hiphop_guy-512.png" alt="..." class="img-circle" height = "150px" width="150px">
 </div>
 </div>
